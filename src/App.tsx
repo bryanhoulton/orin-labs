753879e5e212e875f445997a14a012623f89b600
Bryan Houlton <brhoulton@gmail.com>
2025-12-20 12:17:21 -0500

    fix(cofounder): remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { useEffect } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { useEffect } from 'react';
 
-import Layout from "./components/Layout";
-import Cofounder from "./pages/Cofounder";
-import Home from "./pages/Home";
-import Research from "./pages/Research";
-import ProactiveAgents from "./pages/research/ProactiveAgents";
-import ProactiveVoiceAgents from "./pages/research/ProactiveVoiceAgents";
+import {
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
+
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Research from './pages/Research';
+import ProactiveAgents from './pages/research/ProactiveAgents';
+import ProactiveVoiceAgents from './pages/research/ProactiveVoiceAgents';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -23,47 +27,39 @@ function App() {
     <>
       <ScrollToTop />
       <Routes>
-      <Route
-        path="/"
-        element={
-          <Layout>
-            <Home />
-          </Layout>
-        }
-      />
-      <Route
-        path="/research"
-        element={
-          <Layout>
-            <Research />
-          </Layout>
-        }
-      />
-      <Route
-        path="/research/proactive-agents"
-        element={
-          <Layout>
-            <ProactiveAgents />
-          </Layout>
-        }
-      />
-      <Route
-        path="/research/conversationality"
-        element={
-          <Layout>
-            <ProactiveVoiceAgents />
-          </Layout>
-        }
-      />
-      <Route
-        path="/cofounder"
-        element={
-          <Layout>
-            <Cofounder />
-          </Layout>
-        }
-      />
-    </Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/research"
+          element={
+            <Layout>
+              <Research />
+            </Layout>
+          }
+        />
+        <Route
+          path="/research/proactive-agents"
+          element={
+            <Layout>
+              <ProactiveAgents />
+            </Layout>
+          }
+        />
+        <Route
+          path="/research/conversationality"
+          element={
+            <Layout>
+              <ProactiveVoiceAgents />
+            </Layout>
+          }
+        />
+      </Routes>
     </>
   );
 }
